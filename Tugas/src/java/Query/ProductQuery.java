802f35d13251f37855ebad0e0977e098c7ff4e80
Rifaldlee <103484674+Rifaldlee@users.noreply.github.com>
2022-07-04 17:36:32 +0700

    Update ProductQuery.java

--- Tugas/src/java/Query/ProductQuery.java
+++ Tugas/src/java/Query/ProductQuery.java
@@ -13,7 +13,4 @@ public class ProductQuery {
     public String get = "SELECT * FROM product";
     public String getByName = "SELECT * FROM product where name OR product_type OR merk LIKE ?";
     public String create = "INSERT INTO product (name, product_type, " + "merk, price, release_date) VALUES (?, ?, ?, ?, ?)";
-    public String getById = "SELECT * FROM product where id = ?";   
-    public String update = "UPDATE product SET name = ?, product_type = ?, " + "stock = ?, price = ?, expired = ? WHERE id = ?";
-    public String delete = "DELETE FROM product WHERE id = ?";
 }
